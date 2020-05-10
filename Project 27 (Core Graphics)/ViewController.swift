14d1fa0034dcd51925011bb0db469eaf42d5c3ce
Eddy Ash <eddyashimov@gmail.com>
2020-05-10 14:36:00 -0500

    Initial Commit

---
+++ Project 27 (Core Graphics)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 27 (Core Graphics)
+//
+//  Created by Edil Ashimov on 5/10/20.
+//  Copyright © 2020 Edil Ashimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
